675a9c55c8f9e18b98cc80a0dfe3cd88818f49c4
Nguyễn Văn Tú <vantu1998@gmail.com>
2021-02-23 19:16:39 +0700

    Update cat.js

--- cat.js
+++ cat.js
@@ -2,3 +2,6 @@ function cat(a){
     console.log(a);
 }
 cat("hi every body");
+damp-db-instance-1.cejmnpomi8xv.ap-southeast-1.rds.amazonaws.com
+damp-db-instance-1.cejmnpomi8xv.ap-southeast-1.rds.amazonaws.com
+http://d1dgeurm32dsx0.cloudfront.net/permission-requests/detail/80
